0a0852783a6356f293be9ebe4d1d7a6fc10d4f2e
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2021-06-26 13:08:53 +0530

    second commit
    Took 1 hour 2 minutes

--- app/src/main/java/com/example/tip_time/MainActivity.kt
+++ app/src/main/java/com/example/tip_time/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tip_time
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tip_time.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -19,11 +23,16 @@ class MainActivity : AppCompatActivity() {
             calculateTip()
             calculateTotal()
         }
+
+        binding.costOfServiceEditText.setOnKeyListener {
+                view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
+
         if (cost == null) {
             binding.tipResult.text = ""
             return
@@ -45,8 +54,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTotal() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
+
         if (cost == null) {
             binding.totalResult.text = ""
             return
@@ -66,6 +76,17 @@ class MainActivity : AppCompatActivity() {
 
         val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
         binding.totalResult.text = getString(R.string.total_amount, formattedTotal)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
 
